5f389cff6a6cc46fd994100ae6c0349e12a4474c
Tushar-Thorat-2001 <58701931+Tushar-Thorat-2001@users.noreply.github.com>
2023-07-11 04:59:51 +0530

    first

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Upstyle from "../assets/projects/upstyle.jpg";
+import Data from "../assets/projects/Data.jpg";
+import Node from "../assets/projects/node.jpg";
+import Dalle from "../assets/projects/Dalle.jpg";
+import Portfolio from "../assets/projects/pro.jpg";
+import Doom from "../assets/projects/doom.jpg";
+
+export const data = [
+  {
+    id: 1,
+    name: "UpStyle Ecom",
+    image: Upstyle,
+    github: "https://github.com/Tushar-Thorat-2001/UpStyle",
+    live: "https://upstyle.onrender.com/",
+  },
+  {
+    id: 2,
+    name: "Datafinance",
+    image: Data,
+    github: "https://github.com/Tushar-Thorat-2001/Dataweb",
+    live: "https://react-app-one.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Backend",
+    image: Node,
+    github: "https://github.com/Tushar-Thorat-2001/MENAPI",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "DAll-E Clone",
+    image: Dalle,
+    github: "https://github.com/Tushar-Thorat-2001/Dall-E-Clone",
+    live: "",
+  },
+  {
+    id: 5,
+    name: "Doom Garden",
+    image: Doom,
+    github: "https://github.com/Tushar-Thorat-2001/DOOM-GARDENING",
+    live: "",
+  },
+  {
+    id: 6,
+    name: "Portfolio",
+    image: Portfolio,
+    github: "",
+    live: "/",
+  },
+];
